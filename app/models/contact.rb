f23c300d3383824d0baebabc95d170b12d3f382d
Anthony Galati <anthonygalati01@gmail.com>
2021-08-05 04:22:36 +0000

    Added validation to form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,7 @@
 class Contact < ActiveRecord::Base
-    
+#name,email, commets pulled from db/schema file
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
+
 end    
